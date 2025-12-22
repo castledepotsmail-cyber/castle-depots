8d2794703eed361f427f1afd1bf1d4c77d342363
Castle Depots <castledepotsmail@gmail.com>
2025-12-22 16:00:38 +0300

    Fix: Add logging to AdminOrderViewSet update method to track status changes

--- server/apps/orders/views.py
+++ server/apps/orders/views.py
@@ -19,6 +19,28 @@ class AdminOrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
     permission_classes = [permissions.IsAdminUser]
 
+    def update(self, request, *args, **kwargs):
+        import logging
+        logger = logging.getLogger(__name__)
+        
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        old_status = instance.status
+        
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        new_status = serializer.instance.status
+        logger.info(f"Admin updated order {instance.id}. Status changed: {old_status} -> {new_status}")
+
+        return Response(serializer.data)
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db.models import Sum, Count
